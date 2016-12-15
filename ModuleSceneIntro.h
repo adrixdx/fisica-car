73afbf47aa9eb24b5ce824d535b1959dfff24aeb
adrixdx <adrianhiguerotorres27@gmail.com>
2016-12-15 20:11:53 +0100

    Physics added to circuit

--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -29,7 +29,7 @@ public:
 
 	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
 
-	void CreateCube(float x_scale, float y_scale, float z_scale, Color color, float mass, float x_pos, float y_pos, float z_pos, float degrees, vec3 axis);
+	void CreateCube(float x_scale, float y_scale, float z_scale, vec3 axis, float degrees, vec3 pos, Color color, float mass);
 	void createcube(const float x, const float y, const float z, vec3 axis, float angle, vec3 pos, Color colorcube);
 	void ModuleSceneIntro::createcylinder(const float radius, const float height, vec3 axis, float angle, vec3 pos, Color colorcylinder);
 
