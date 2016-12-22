a36c7a1a243832eb06b5d7575c02f7b2f09d949c
Yabel Rodríguez <yabelrr@gmail.com>
2016-12-22 09:47:36 +0100

    Game

--- Globals.h
+++ Globals.h
@@ -37,5 +37,5 @@ enum update_status
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
 #define TITLE "3D Physics Playground"
-#define LAPS 1
-#define TIMER 100
+#define LAPS 3
+#define TIMER 90
